b13387f835ac2ef8807cc22072b073219d94259f
equation9 <equation9@naver.com>
2017-03-30 08:51:51 -0700

    3/30 commit2

---
+++ 1402.cpp
@@ -0,0 +1,14 @@
+/*1402번 아무래도이문제는A번난이도인것같다(C++11)*/
+#include <cstdio>
+
+int main()
+{
+	int T, A, B;
+	scanf("%d", &T);
+	while(T--)
+	{
+		scanf("%d %d", &A, &B);
+		printf("yes\n");
+	}
+	return 0;
+}
